8ac4290365688d6326c1f556fce1154d56ec660e
Wahid Shalaly <waheedsayed@gmail.com>
2014-08-11 12:03:04 +1000

    Installed GitVersionTask 1.1.0

--- src/ReliableUnitOfWork.SqlAzure/Properties/AssemblyInfo.cs
+++ src/ReliableUnitOfWork.SqlAzure/Properties/AssemblyInfo.cs
@@ -22,18 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0d432c83-14fc-4de0-9d51-adc655ccc6a1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 // CA1014: Mark assemblies with CLSCompliantAttribute
 [assembly: CLSCompliant(true)]
